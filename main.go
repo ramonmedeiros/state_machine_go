e63b9afce4b211841cd73ec93041457a0c75949c
Ramon Medeiros <ramon.rnm@gmail.com>
2019-07-24 23:39:07 +0200

    Write main.go

--- main.go
+++ main.go
@@ -3,5 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello")
+	fmt.Println(`State machines usually create structs with methods IsValid(), Action() and Next(), so you can run them in a loop like this:
+while state.Valid() == true {
+    state.Action()
+    state = state.Next()
+}
+
+Instead of creating a interactive flow like this, I created a lot of tests in states module and a flow test in this folder, so we can see the progres.
+
+I suggest to run make in root folder and look for the logs :) `)
 }
